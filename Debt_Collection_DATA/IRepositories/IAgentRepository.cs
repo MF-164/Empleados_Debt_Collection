d56e4fa2de4140a6e1ac1f242688efad6b57804f
MALY <malyfertig@gmail.com>
2025-07-14 20:56:37 +0300

    adding basic repositories of models

---
+++ Debt_Collection_DATA/IRepositories/IAgentRepository.cs
@@ -0,0 +1,23 @@
+﻿using Debt_Collection_DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debt_Collection_DATA.IRepositories
+{
+    public interface IAgentRepository
+    {
+        //Create
+        Task CreateAsync(Agent newAgent);
+
+        //Read
+        Task<Agent> GetByIdAsync(int agentId);
+        Task<IEnumerable<Agent>> GetByManagersAsync(IEnumerable<int> managers);
+       
+        //Update
+
+        //Delete
+    }
+}
